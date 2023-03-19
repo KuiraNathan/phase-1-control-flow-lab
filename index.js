70cfe6f07ac888093bf34627f3730cc6a34c5f1b
KuiraNathan <nathanmwangi70@gmail.com>
2023-03-19 23:11:24 +0300

    my control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+  if (distance <= 400){
+   return 'This one is on me!';
+  }else if (distance > 2000 && distance <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good."
+  :"No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  console.log(tip);
+  let message;
+  switch(tip){
+    case "generous":
+      message ="Thank you so much.";
+      break;
+
+      case ("not as generous"):
+        //console.log(tip)
+        message= 'Thank you.'
+        break;
+
+        case ("not as generous"):
+          //console.log(tip);
+          message= 'Thank you.'
+          break;
+
+          default:
+            message= 'Bye.'
+            console.log('Bye.');
+  }
+return message
 }
